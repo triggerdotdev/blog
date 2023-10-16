cef1e3b4916c51f2318c9a656a0cefd45247b2c7
James Ritchie <james@jamesritchie.co.uk>
2023-10-16 16:44:07 +0100

    added async await

--- avatar-generator/src/pages/api/generate.ts
+++ avatar-generator/src/pages/api/generate.ts
@@ -2,61 +2,57 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import formidable from "formidable";
 import { client } from "@/trigger";
-import {Writable} from "stream";
+import { Writable } from "stream";
 
 export const config = {
-	api: {
-		bodyParser: false,
-	},
+  api: {
+    bodyParser: false,
+  },
 };
 
 const fileConsumer = (acc: any) => {
-	const writable = new Writable({
-		write: (chunk, _enc, next) => {
-			acc.push(chunk);
-			next();
-		},
-	});
+  const writable = new Writable({
+    write: (chunk, _enc, next) => {
+      acc.push(chunk);
+      next();
+    },
+  });
 
-	return writable;
+  return writable;
 };
 
-const readFile = (
-	req: NextApiRequest,
-	saveLocally?: boolean
-) => {
-	// @ts-ignore
-	const chunks: any[] = [];
-	const form = formidable({
-		keepExtensions: true,
-		fileWriteStreamHandler: () => fileConsumer(chunks),
-	});
+const readFile = (req: NextApiRequest, saveLocally?: boolean) => {
+  // @ts-ignore
+  const chunks: any[] = [];
+  const form = formidable({
+    keepExtensions: true,
+    fileWriteStreamHandler: () => fileConsumer(chunks),
+  });
 
-	return new Promise((resolve, reject) => {
-		form.parse(req, (err, fields: any, files: any) => {
-			const image = Buffer.concat(chunks).toString('base64');
-			client.sendEvent({
-				name: "generate.avatar",
-				payload: {
-					image,
-					email: fields.email[0],
-					gender: fields.gender[0],
-					userPrompt: fields.userPrompt[0],
-				},
-			});
+  return new Promise((resolve, reject) => {
+    form.parse(req, async (err, fields: any, files: any) => {
+      const image = Buffer.concat(chunks).toString("base64");
+      await client.sendEvent({
+        name: "generate.avatar",
+        payload: {
+          image,
+          email: fields.email[0],
+          gender: fields.gender[0],
+          userPrompt: fields.userPrompt[0],
+        },
+      });
 
-			if (err) reject(err);
-			resolve({ fields, files });
-		});
-	});
+      if (err) reject(err);
+      resolve({ fields, files });
+    });
+  });
 };
 
 export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
+  await readFile(req, true);
 
-	await readFile(req, true);
-
-	res.status(200).json({ message: "Processing!" });
+  res.status(200).json({ message: "Processing!" });
 }
