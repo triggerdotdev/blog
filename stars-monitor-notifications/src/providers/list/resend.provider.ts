75399556607673e0481e11b91d660fa54a295f8a
Nevo David <nevo@notu.co>
2023-11-19 16:51:20 +0700

    feat: eric

--- stars-monitor-notifications/src/providers/list/resend.provider.ts
+++ stars-monitor-notifications/src/providers/list/resend.provider.ts
@@ -1,6 +1,5 @@
 import {object, string} from "zod";
 import {registerProvider} from "@/providers/register.provider";
-import axios from "axios";
 import { Resend } from 'resend';
 
 export const ResendProvider = registerProvider(
@@ -12,12 +11,10 @@ export const ResendProvider = registerProvider(
     async (libName, stars, values) => {
         const resend = new Resend(values.RESEND_API_KEY);
         await resend.emails.send({
-            from: "Nevo David <nevo@gitup.dev>",
-            to: ['nevo@gitroom.com'],
+            from: "Eric Allam <eric@trigger.dev>",
+            to: ['eric@trigger.dev'],
             subject: 'New GitHub stars',
             html: `The library ${libName} has ${stars} new stars!`,
         });
-
-        await axios.post(values.DISCORD_WEBHOOK_URL, {content: `The library ${libName} has ${stars} new stars!`});
     }
 );
